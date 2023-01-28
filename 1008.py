4a440bfde03bc84e70fc50ad96edec285927dfb8
Md. Mustafizur Rahman <119804176+mustafizur85@users.noreply.github.com>
2023-01-28 17:52:46 +0600

    Add files via upload

---
+++ 1008.py
@@ -0,0 +1,11 @@
+
+EmployNumber = int(input())
+EmployWorkHourNumber = int(input())
+AmountRecivePerHour = float(input())
+
+SALARY = EmployWorkHourNumber * AmountRecivePerHour
+
+print("NUMBER =",EmployNumber)
+
+print('SALARY = U$ %.2f'% SALARY)
+
